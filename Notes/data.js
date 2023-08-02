393b3aefb4d3d2d5f7a280dfdd0339b1776cc16b
ihor <vasilyev.igor18@gmail.com>
2023-08-02 22:46:21 +0100

    initial Notes app
    (cherry picked from commit dd7fdb02ba6d7a31774ba4a818b399772a5c7b4b)

---
+++ Notes/data.js
@@ -0,0 +1,46 @@
+let notes = [
+    {
+        time: '2023-08-01T12:00:00',
+        name: "Groceries",
+        content: "I'm going to buy groceries.",
+        category: "Task",
+    },
+    {
+        time: '2023-08-01T12:00:00',
+        name: "Gijfj",
+        content: "I'm going to buy groceries.",
+        category: "Task",
+    },
+    {
+        time: '2023-08-01T12:00:00',
+        name: "bbbfbfbf",
+        content: "I'm going to buy groceries.",
+        category: "Task",
+    },
+    {
+        time: '2023-08-01T12:00:00',
+        name: "lflflf",
+        content: "I'm going to buy groceries.",
+        category: "Task",
+    },
+    {
+        time: '2023-08-01T12:00:00',
+        name: "uytyre",
+        content: "I have a dentist appointment on 5/8/2023.",
+        category: "Task",
+    },
+    {
+        time: '2023-08-01T12:00:00',
+        name: "blfikf",
+        content: "I'm going to buy groceries.",
+        category: "Task",
+    },
+    {
+        time: '2023-08-01T12:00:00',
+        name: "rrtyujg",
+        content: "I moved the dentist appointment from 5/8/2023 to 8/8/2023.",
+        category: "Task",
+    },
+];
+
+
