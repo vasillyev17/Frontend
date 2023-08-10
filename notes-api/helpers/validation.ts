adf6c2e50b5fdc1fe296b00d5a4c29a9d4da8e1b
ihor <vasilyev.igor18@gmail.com>
2023-08-10 20:11:59 +0100

    simplify validation

--- notes-api/helpers/validation.ts
+++ notes-api/helpers/validation.ts
@@ -3,8 +3,6 @@ import { Note } from './note';
 export const validateNote = (note: Note) => {
 	console.log(note);
 	const isValidTime = (new Date(note.time)).toString() !== 'Invalid Date';
-	if (note.name && note.time && note.category && note.content && note.time && isValidTime) {
-		return true;
-	}
-	return false;
+	return note.name && note.time && note.category && note.content && note.time && isValidTime;
+
 };
